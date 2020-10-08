8cf81840c9c59209f517553e1ed35e25729ba3e9
Corefracture <corefracture@corefracture.com>
2020-10-08 16:42:58 -0400

    [cf] - Some quick additions

--- UIScripts_CustomDialog.js
+++ UIScripts_CustomDialog.js
@@ -7,12 +7,23 @@ excludes:
 
 js:*/
 
+class CFUIScriptsDialog {
+    constructor(options) {
 
-var baseWin =   '       <div class="dialog ui-dialog-content ui-widget-content"  '  +
-                '            style="width: 90%; display: block; min-height: 100px; height: auto; padding: 10px">  '  +
-                '            <div style="float: bottom; width: 100%">' +
+    }
+}
+mycar = new Car("Ford");
+
+var baseWin =   '       <div class="dialog ui-dialog-content ui-widget-content"'  +
+                '           style="width: 90%; display: block; min-height: 100px; height: auto; padding: 10px">'  +
+                '           <div id="cf-uiscripts-dialog-controls">' +
+                '           </div>' +
+                '           <hr/>' +
+                '           <div style="float: bottom; width: 100%">' +
                 '               <label for="cf-uiscripts-dialog-log">Operation Log:</label>' +
-                '               <textarea id="cf-uiscripts-dialog-log" readonly ' +
-                '              style="width: 100%; height: 80px; class="form-control form-control-full processed textarea-with-grippie">Test</textarea>'  +
-                '            </div>' +
+                '               <textarea id="cf-uiscripts-dialog-log" readonly' +
+                '               style="width: 100%; height: 80px; background-color: #f1f1f1;"' +
+                '               class="form-control form-control-full processed textarea-with-grippie">Test</textarea>'  +
+                '           </div>' +
                 '       </div>  ';
+
